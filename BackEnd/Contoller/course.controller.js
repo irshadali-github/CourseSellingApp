409b02b8efdddb9c2a9d1e046d878e35b14d7085
irshad <irshad.ali052@gmail.com>
2025-01-15 20:11:33 +0530

    GetallCourses and oneCourse controller is created

--- BackEnd/Contoller/course.controller.js
+++ BackEnd/Contoller/course.controller.js
@@ -14,7 +14,18 @@ export const getCourses = async (req, res) => {
   }
 };
 
-
+export const courseDetail = async (req, res) => {
+  try{
+    const {courseId}=req.params;
+    const course=await Course.findById(courseId);
+    if(!course){
+      return res.status(404).json({errors:"Course Not found"});
+    }
+    res.status(200).json({course});
+  }catch(error){
+    res.status(500).json({error:"Error in finding Course"})
+  }
+};
 
 export const createCourse = async (req, res) => {
   const { title, description, price } = req.body;
--- BackEnd/Routes/course.routes.js
+++ BackEnd/Routes/course.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createCourse, deleteCourse, getCourses, updateCourse } from '../Contoller/course.controller.js';
+import { courseDetail, createCourse, deleteCourse, getCourses, updateCourse } from '../Contoller/course.controller.js';
 const router=express.Router();
 
 router.get('/courses',getCourses);
+router.get('/:courseId',courseDetail)
 router.post('/create',createCourse);
 router.put('/update/:courseId',updateCourse);
 router.delete('/delete/:courseId',deleteCourse);
